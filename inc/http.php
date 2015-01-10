f391948726aee08d257b269f3035aeba83a17758
Tony Yip <tonyhhyip@gmail.com>
2015-01-10 12:55:14 +0800

    Add the http.php to send header.

---
+++ inc/http.php
@@ -0,0 +1,9 @@
+<?php
+	ob_start();
+	header("X-UA-Compatible: IE=9; IE=8; IE=7; IE=EDGE");
+	header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
+	header("Content-Type: text/html; charset=utf-8");
+	header("Pragma: no-cache");
+	header("Refresh: 600");
+	require_once("SilverSnake/init.php");
+?>
